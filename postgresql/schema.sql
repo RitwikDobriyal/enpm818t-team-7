1a10ace5a5889e16d48d54cf99f845fd4dbe2f12
RitwikDobriyal <ritwikdobriyal54@gmail.com>
2026-04-16 00:29:58 -0400

    started schema.sql -- custom enums

--- postgresql/schema.sql
+++ postgresql/schema.sql
@@ -9,3 +9,15 @@
 --      UNIQUE constraints: All candidate keys
 --      Indexes: Strategic indexes for query performance
 --      Triggers: Automatic updated_at timestamps
+
+
+-- Creating custom types (ENUMs for constrained values), from feedback w/GP1
+CREATE TYPE intersection_type AS ENUM ('signalized', 'unsignalized', 'roundabout');
+CREATE TYPE signal_approach AS ENUM ('N', 'S', 'E', 'W');
+CREATE TYPE signal_timing_mode AS ENUM ('fixed', 'adaptive', 'emergency');
+CREATE TYPE signal_hardware_type AS ENUM ('LED', 'incandescent', 'pedestrian')
+CREATE TYPE sensor_type AS ENUM ('inductive_loop', 'radar', 'camera', 'lidar', 'acoustic');
+CREATE TYPE signal_status AS ENUM ('operational', 'inactive', 'maintenance');
+CREATE TYPE road_surface AS ENUM('asphalt', 'concrete', 'gravel');
+CREATE TYPE incident_severity AS ENUM ('minor', 'moderate', 'major', 'critical');
+CREATE TYPE incident_category_type AS ENUM('accident', 'breakdown', 'hazard', 'construction', 'event');
